f8600ce1b00e2dd0b304386be44adf8786a643c8
mmloveaa <mandychan1128@gmail.com>
2016-04-23 23:04:08 -0700

    added POST and DELETE

--- routes/agency.js
+++ routes/agency.js
@@ -6,4 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+r
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,36 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+router.delete('/logout', function(req, res) {
+  res.clearCookie('mandycookie').send();
+});
+
+router.get('/profile', User.authMiddleware, function(req, res) {
+  res.send(req.user);
+});
+
+router.post('/authenticate', function(req, res) {
+  User.authenticate(req.body, function(err, user) {
+    if(err) {
+      res.status(400).send(err);
+    } else {
+      var token = user.generateToken();
+      res.cookie('mandycookie', token).send(user);
+    }
+  });
+});
+
+router.post('/register', function(req, res) {
+  User.register(req.body, function(err, user) {
+    if(err) {
+      res.status(400).send(err);
+    } else {
+      var token = user.generateToken();
+      res.cookie('mandycookie', token).send(user);
+    }
+  });
+});
+
 module.exports = router;
