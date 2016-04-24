7ea441d8b57147344d655d1f0bf0e76b907263b6
mmloveaa <mandychan1128@gmail.com>
2016-04-23 22:13:24 -0700

    added AuthService

--- public/js/services.js
+++ public/js/services.js
@@ -1,1 +1,32 @@
 "use strict";
+
+app.service('AuthService', function($http, $state, UserService) {
+  this.register = function(user) {
+    return $http.post('/users/register', user)
+      .then(function(res) {
+        UserService.set(res.data);
+      });
+  };
+
+  this.login = function(user) {
+    return $http.post('/users/authenticate', user)
+      .then(function(res) {
+        UserService.set(res.data);
+      });
+  };
+
+  this.logout = function() {
+    $http.delete('/users/logout')
+    .then(function() {
+      UserService.destroy();
+      $state.go('home')
+    });
+  };
+
+  this.init = function() {
+    $http.get('/users/profile')
+    .then(function(res) {
+      UserService.set(res.data);
+    });
+  };
+});
