d5e4fd35819d51ac09a08c5115c2e343c6ca3314
mmloveaa <mandychan1128@gmail.com>
2016-04-23 20:47:45 -0700

    updated profile and nav ctrl

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,1 +1,57 @@
 "use strict";
+
+var app = angular.module('userAuth');
+
+app.controller('profileCtrl', function($scope, UserService, $http) {
+  console.log("userService: ", UserService);
+
+  $scope.$watch(function(){
+    return UserService.image;
+  },function(curr,prev){
+      $scope.image = curr;
+  });
+
+  $scope.update = function(newUser) {
+    console.log();
+    UserService.update(newUser);
+  }
+})
+
+app.controller('navCtrl', function($scope, UserService, AuthService) {
+  $scope.logout = function() {
+    AuthService.logout();
+  };
+  $scope.$watch(function() {
+    return UserService.username;
+  }, function(username) {
+    $scope.username = username;
+  });
+});
+
+app.controller('authCtrl', function($scope, $state, AuthService) {
+  $scope.state = $state.current.name;
+  $scope.submit = function(user) {
+    if($scope.state === 'register') {
+      // submit register form
+      if(user.password !== user.password2) {
+        $scope.user.password = $scope.user.password2 = '';
+        alert('HEY. Passwords gotta match!');
+      } else {
+        AuthService.register(user)
+          .then(function() {
+            $state.go('home');
+          }, function(err) {
+            console.error(err);
+          });
+      }
+    } else {
+      // submit login form
+      AuthService.login(user)
+        .then(function() {
+          $state.go('home');
+        }, function(err) {
+          console.error(err);
+        });
+    }
+  };
+});
